29838c48e53b88d7bc04de15532e5d269c03e30e
Brynzananas <brynzananas@gmail.com>
2026-01-05 15:46:44 +0300

    fuck you monomod backports

--- CaeliImperium/CaeliImperiumPlugin.cs
+++ CaeliImperium/CaeliImperiumPlugin.cs
@@ -25,7 +25,7 @@ namespace CaeliImperium
     {
         public const string ModGuid = "com.brynzananas.caeliimperium";
         public const string ModName = "Caeli Imperium";
-        public const string ModVer = "0.8.1";
+        public const string ModVer = "0.8.2";
         public const string ModPrefix = "CI";
         public static bool emotesEnabled;
         public static bool riskOfOptionsEnabled;
